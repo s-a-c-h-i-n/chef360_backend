406b14828fdf655c3718a9da30188f7a5b5e0c85
RemondXie <149639223+RemondXie@users.noreply.github.com>
2023-11-07 14:03:55 -0500

    Update app.py

--- db_helper/db_python_app_template/app.py
+++ db_helper/db_python_app_template/app.py
@@ -8,13 +8,13 @@ from flask import Flask, redirect, render_template, request, url_for, jsonify
 #from typing import Union
 import sys
 sys.path.append('./tools')
-
+import time
 
 from pydantic import BaseModel
 from flask_pydantic import validate
-import jwtTool
-import stringOperator
-from returnPattern import errorReturn
+import tools.jwtTool as jwtTool
+import tools.stringOperator as stringOperator
+from tools.returnPattern import errorReturn
 
 # Create an instance for the web App
 app = Flask(__name__)
@@ -22,6 +22,8 @@ app = Flask(__name__)
 load_dotenv()
 connection_string = os.getenv("AZURE_SQL_CONNECTIONSTRING")
 response = ''
+
+start=2
 
 class Customer(BaseModel):
     first_name: str
@@ -43,8 +45,9 @@ def getTestToken():
     token=jwtTool.generateToken(data)
     print("generated test token:",token)
     return token
-@app.route("/api/getPreference",methods=["POST"])
-def getPreference():
+
+@app.route("/api/getUserInformation",methods=['POST'])
+def getUserInformation():
     argsJson = request.data.decode('utf-8')
     argsJson = json.loads(argsJson)
     token=argsJson['token']
@@ -57,69 +60,66 @@ def getPreference():
         return errorReturn(400,"This user already expired")
     with get_conn() as conn:
         cursor = conn.cursor()
-        row=cursor.execute("SELECT USERID,COOKWARE,ALLERGICS FROM dbo.USER_PREFERENCE WHERE USERID=?",data['USERID']).fetchone()
+        row=cursor.execute("SELECT USERID,USERNAME,USER_PASSWORD,FULL_NAME,LAST_NAME,E_MAIL,PHONE_NUMBER,REGISTRATION_TIME,LAST_LOGIN_TIME,USER_STATUS,USER_TYPE,COUNTRY,CITY FROM dbo.USER_PROFILE WHERE USERID=?",data['USERID']).fetchone()
         if(row is None):
             return errorReturn(404,"Not found")
-        row.ALLERGICS=stringOperator.splitUnderscores(row.ALLERGICS)
-        row.COOKWARE=stringOperator.splitUnderscores(row.COOKWARE)
+        row.USERID = row.USERID
+        row.USERNAME=stringOperator.splitUnderscores(row.USERNAME)
+        row.FULL_NAME = stringOperator.splitUnderscores(row.FULL_NAME)
+        row.LAST_NAME = stringOperator.splitUnderscores(row.LAST_NAME)
+        row.E_MAIL = stringOperator.splitUnderscores(row.E_MAIL)
+        row.PHONE_NUMBER = row.PHONE_NUMBER
+        row.REGISTRATION_TIME = row.REGISTRATION_TIME
+        row.LAST_LOGIN_TIME = row.LAST_LOGIN_TIME
+        row.USER_STATUS = stringOperator.splitUnderscores(row.USER_STATUS)
+        row.USER_TYPE = stringOperator.splitUnderscores(row.USER_TYPE)
+        row.COUNTRY = stringOperator.splitUnderscores(row.COUNTRY)
+        row.CITY = stringOperator.splitUnderscores(row.CITY)
         print(row)
         ret={
             "code":200,
             "message":"success",
             "data":{
                 "USERID":row.USERID,
-                "COOKWARE":row.COOKWARE,
-                "ALLERGICS":row.ALLERGICS
+                "USERNAME":row.USERNAME,
+                "FULL_NAME":row.FULL_NAME,
+                "LAST_NAME": row.LAST_NAME,
+                "E_MAIL": row.E_MAIL,
+                "PHONE_NUMBER": row.PHONE_NUMBER,
+                "REGISTRATION_TIME": row.REGISTRATION_TIME,
+                "LAST_LOGIN_TIME": row.LAST_LOGIN_TIME,
+                "USER_STATUS": row.USER_STATUS,
+                "USER_TYPE": row.USER_TYPE,
+                "COUNTRY": row.COUNTRY,
+                "CITY": row.CITY,
             }
         }
         return ret
-
     return errorReturn(404,"Not found")
-@app.route("/api/addOrUpdatePreference",methods=["POST"])
-def addOrUpdatePreference():
-    argsJson = request.data.decode('utf-8')
-    argsJson = json.loads(argsJson)
-    token=argsJson['token']
-    data,expired,state,message=jwtTool.decodeToken(token)
-    if(state==False):
-        if(str(message)=="Signature has expired"):
-            return errorReturn(400,str(message))
-        return errorReturn(300,str(message))
+
+@app.route("/api/Register",methods=['POST'])
+def Register():
+    USERNAME=request.form['USERNAME']
+    USER_PASSWORD = request.form['USER_PASSWORD']
+    FULL_NAME = request.form['FULL_NAME']
+    LAST_NAME = request.form['LAST_NAME']
+    E_MAIL = request.form['E_MAIL']
+    PHONE_NUMBER = request.form['PHONE_NUMBER']
+    COUNTRY = request.form['COUNTRY']
+    CITY = request.form['CITY']
+    USER_STATUS = 'ONLINE'
+    USER_TYPE = 'REGULAR_USER'
+    REGISTRATION_TIME = time.strftime("%Y-%m-%d %H:%M:%S",time.localtime())
+    LAST_LOGIN_TIME = time.strftime("%Y-%m-%d %H:%M:%S",time.localtime())
+    global start
     with get_conn() as conn:
         cursor = conn.cursor()
-        row=cursor.execute("SELECT USERID,COOKWARE,ALLERGICS FROM dbo.USER_PREFERENCE WHERE USERID=?",data['USERID']).fetchone()
-        if(row is None):
-            sql = "INSERT INTO dbo.USER_PREFERENCE (USERID, COOKWARE,ALLERGICS) VALUES (?, ?, ?)"
-            cookware=stringOperator.mergeUnderscores(argsJson['cookware'])
-            allergics=stringOperator.mergeUnderscores(argsJson['allergics'])
-            print(cookware)
-            print(allergics)
-            cursor.execute(sql, data['USERID'],cookware,allergics)
-            conn.commit()
-        else:
-            sql = "UPDATE dbo.USER_PREFERENCE SET COOKWARE=?,ALLERGICS=?  WHERE USERID=?"
-            cookware=stringOperator.mergeUnderscores(argsJson['cookware'])
-            allergics=stringOperator.mergeUnderscores(argsJson['allergics'])
-            print(cookware)
-            print(allergics)
-            cursor.execute(sql, cookware,allergics,data['USERID'])
-            conn.commit()
-        ret={
-            "code":200,
-            "message":"success",
-            "data":{
-            }
-        }
-        return ret
-    return errorReturn(500,"Error occur")
-
-
-    
-
-
-
+        cursor.execute("INSERT INTO dbo.USER_PROFILE VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)",(start,USERNAME,USER_PASSWORD,FULL_NAME,LAST_NAME,E_MAIL,PHONE_NUMBER,REGISTRATION_TIME,LAST_LOGIN_TIME,USER_STATUS,USER_TYPE,COUNTRY,CITY))
+        start+=1
+        conn.commit()
+    return USERNAME
 
 
     
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
