19608c33694409425a383f69688bb3e88cf432a3
RemondXie <149639223+RemondXie@users.noreply.github.com>
2023-11-10 19:54:06 -0500

    Update auth.py

--- routes/auth.py
+++ routes/auth.py
@@ -1,6 +1,13 @@
 from flask import Blueprint
 from services.authService import login
+from services.authService import register
+from services.authService import getPersonalInfor
+
 
 auth_bp = Blueprint('auth_bp', __name__)
 
 auth_bp.route('/login', methods=['POST'])(login)
+
+auth_bp.route('/register', methods=['POST'])(register)
+
+auth_bp.route('/getPersonalInfor', methods=['POST'])(getPersonalInfor)
