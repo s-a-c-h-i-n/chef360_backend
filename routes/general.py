c0ede9f40a7f682fd6616d0eb9d86de727ef7e27
RemondXie <149639223+RemondXie@users.noreply.github.com>
2023-11-10 19:54:47 -0500

    Update general.py

--- routes/general.py
+++ routes/general.py
@@ -1,8 +1,18 @@
 from flask import Blueprint, render_template
 from services.generalService import index
+from services.generalService import register
+from services.generalService import getPersonalInfor
 
 general_bp = Blueprint('general_bp', __name__)
 
 @general_bp.route('/')
 def landingPage():
     return index()
+
+@general_bp.route('/register')
+def registerpage():
+    return register()
+
+@general_bp.route('/getPersonalInfor')
+def getPersonalInfor():
+    return getPersonalInfor()
