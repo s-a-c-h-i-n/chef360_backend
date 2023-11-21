62c35cd4ac435e335285ecd916a0d490ba8304fb
1127568237@qq.com <43734311+Revolvor@users.noreply.github.com>
2023-11-21 10:27:38 -0500

    store recipe

--- routes/recipe.py
+++ routes/recipe.py
@@ -1,7 +1,8 @@
 from flask import Blueprint
-from services.recipeService import recipePromptGeneration
+from services.recipeService import recipePromptGeneration,storeRecipe
 
 
 recipe_bp = Blueprint('recipe_bp', __name__)
 
 recipe_bp.route('/recipePrompt', methods=['POST'])(recipePromptGeneration)
+recipe_bp.route("/storeRecipe",methods=['POST'])(storeRecipe)
