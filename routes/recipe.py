d5739e046682ad09eb19bb5ade3717c643123580
RemondXie <149639223+RemondXie@users.noreply.github.com>
2023-11-21 20:10:28 -0500

    Update recipe.py

--- routes/recipe.py
+++ routes/recipe.py
@@ -4,5 +4,5 @@ from services.recipeService import recipePromptGeneration,storeRecipe
 
 recipe_bp = Blueprint('recipe_bp', __name__)
 
-recipe_bp.route('/recipePrompt', methods=['POST'])(recipePromptGeneration)
+
 recipe_bp.route("/storeRecipe",methods=['POST'])(storeRecipe)
