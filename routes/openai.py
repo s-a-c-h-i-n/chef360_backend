d7645b7bd5ebae898d89c3ebef7d44e8dca36259
RemondXie <149639223+RemondXie@users.noreply.github.com>
2023-11-24 15:29:01 -0500

    Update openai.py

--- routes/openai.py
+++ routes/openai.py
@@ -1,8 +1,8 @@
 from flask import Blueprint
-from services.openaiService import getCompletion
+from services.openaiService import generateRecipe
 from services.openaiService import getRecipe
 
 
 openai_bp = Blueprint('openai_bp', __name__)
 
-openai_bp.route('/getCompletion', methods=['POST'])(getCompletion)
+openai_bp.route('/generateRecipe', methods=['POST'])(generateRecipe)
