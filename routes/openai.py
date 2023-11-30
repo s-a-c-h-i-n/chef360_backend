d01b85b1da25ee908a92dd10ee36fe267be62d64
NDCorp <ndcorp13@gmail.com>
2023-11-29 23:54:51 -0500

    Update openai.py
    Adding createRecipePrompt

--- routes/openai.py
+++ routes/openai.py
@@ -1,8 +1,9 @@
 from flask import Blueprint
-from services.openaiService import generateRecipe
+from services.openaiService import generateRecipe, createRecipePrompt
 from services.openaiService import getRecipe
 
 
 openai_bp = Blueprint('openai_bp', __name__)
 
 openai_bp.route('/generateRecipe', methods=['POST'])(generateRecipe)
+openai_bp.route('/createRecipePrompt', methods=['GET'])(createRecipePrompt)
